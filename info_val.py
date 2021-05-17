39516aebb7cab9adc1ba6c9e6a91ca5601d8f5aa
Kurihiro <54324711+kurihiro0119@users.noreply.github.com>
2021-05-17 19:30:02 +0900

    key 削除

--- info_val.py
+++ info_val.py
@@ -2,10 +2,10 @@
 # coding: UTF-8
 
 #情報
-Consumer_key = 'gV9rrGZhUbU1QCujMPhYxCGVD'
-Consumer_secret = 'rPNQCr0pwWRyUKRSuZSQZ2PVd1SkfRIoNlZbmqY5RVw7wyKfjn'
-Access_token = '154103818-fe4tV19Khef68fKO3KRfypbFtHQSBQW1rHOWumbG'
-Access_secret = 'fUoAAVVGgxHU2ENZC5ZbG9AgA2s57qjYKbPAoD6mbV2IN'
-text_analytics_base_url = "https://kuri-text-analytics.cognitiveservices.azure.com/"
+Consumer_key = ''
+Consumer_secret = ''
+Access_token = ''
+Access_secret = ''
+text_analytics_base_url = ""
 sentiment_api_url = text_analytics_base_url + "text/analytics/v3.0/languages"
-subscription_key = "44fa217b94d2474e8b8cce68d7e1f1c1"
+subscription_key = ""
